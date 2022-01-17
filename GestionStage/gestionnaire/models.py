129c36654678f6cab00c8ec4b35a423b7dc3f5ff
Abdenour Touat <61945965+abdenour-progdes@users.noreply.github.com>
2022-01-17 16:44:19 +0100

    abdenour added the Group class and the Stagier class you need to create the Stage,Promoteur and Encadreur

--- GestionStage/gestionnaire/models.py
+++ GestionStage/gestionnaire/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Stagier(models.Model):
+    nom = models.CharField(max_length=120)
+    prenom =models.CharField(max_length=120)
+    niveauDetude = models.CharField(max_length=80)
+    idGroup = models.ForeignKey(Group, on_delete=models.CASCADE)
+ 
+class Group(models.Model):
+    dateStage = models.DateField()
+    numStage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+    idPromoteur = models.ForeignKey(Promoteur, on_delete=models.CASCADE)
+    numEncadreur = models.ForeignKey(Encadreur, on_delete=models.CASCADE)
