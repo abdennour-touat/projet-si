91850c36470508ccc4a747cb238700fa17047d7f
xlxys <76634908+xlxys@users.noreply.github.com>
2022-01-17 19:39:33 +0100

    Update models.py

--- GestionStage/gestionnaire/models.py
+++ GestionStage/gestionnaire/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Promoteur(models.Model):
     nom = models.CharField(max_length=120)
-    prenom =models.CharField(max_length=120)
+    prenom = models.CharField(max_length=120)
 
 
 class Encadreur(models.Model):
     nom = models.CharField(max_length=120)
-    prenom =models.CharField(max_length=120)
+    prenom = models.CharField(max_length=120)
     idOrganisme = models.ForeignKey(Organisme, on_delete=models.CASCADE)
 
  
@@ -21,6 +21,6 @@ class Groupe(models.Model):
 
 class Stagier(models.Model):
     nom = models.CharField(max_length=120)
-    prenom =models.CharField(max_length=120)
+    prenom = models.CharField(max_length=120)
     niveauDetude = models.CharField(max_length=80)
     idGroupe = models.ForeignKey(Groupe, on_delete=models.CASCADE)
