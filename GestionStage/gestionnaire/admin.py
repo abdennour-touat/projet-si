9447c9bde2b1568193252fe7883ff8740270217b
xlxys <76634908+xlxys@users.noreply.github.com>
2022-01-18 14:45:08 +0100

    all models working
    all models working correctly
    add data to data base
    added email and phone number to promoteur/ stagier /encadreur

--- GestionStage/gestionnaire/admin.py
+++ GestionStage/gestionnaire/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Encadreur, Promoteur, Groupe, Stagier
+from .models import Encadreur, Promoteur, Groupe, Stagier, Organisme, Stage, typeStage
 
+
+admin.site.register(Organisme)
 admin.site.register(Encadreur)
 admin.site.register(Promoteur)
 admin.site.register(Groupe)
 admin.site.register(Stagier)
+admin.site.register(typeStage)
+admin.site.register(Stage)
+
--- GestionStage/gestionnaire/models.py
+++ GestionStage/gestionnaire/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
 
+class Organisme(models.Model):
+    nomOrganisme=models.CharField(max_length=120) 
+    typeOrganisme=models.CharField(max_length=30) 
+
+class typeStage(models.Model):
+    typeStage=models.CharField(max_length=30)
+    duree=models.IntegerField(help_text="Semaine")
+
+class Stage(models.Model):
+    nomStage=models.CharField(max_length=120)
+    typeStage=models.ForeignKey(typeStage, on_delete=models.CASCADE)
+    idOrganisme=models.ForeignKey(Organisme, on_delete=models.CASCADE)
+
 class Promoteur(models.Model):
     nom = models.CharField(max_length=120)
     prenom = models.CharField(max_length=120)
-
+    email = models.CharField(max_length=120)
+    numeroTelephone = models.CharField(max_length=120)
 
 class Encadreur(models.Model):
     nom = models.CharField(max_length=120)
     prenom = models.CharField(max_length=120)
     idOrganisme = models.ForeignKey(Organisme, on_delete=models.CASCADE)
+    email = models.CharField(max_length=120)
+    numeroTelephone = models.CharField(max_length=120)
 
  
 class Groupe(models.Model):
@@ -24,17 +40,7 @@ class Stagier(models.Model):
     prenom = models.CharField(max_length=120)
     niveauDetude = models.CharField(max_length=80)
     idGroupe = models.ForeignKey(Groupe, on_delete=models.CASCADE)
+    email = models.CharField(max_length=120)
+    numeroTelephone = models.CharField(max_length=120)
 
-class Organisme(models.Model):
-    
-    nomOrganisme=models.CharField(max_length=120) 
-    typeOrganisme=models.CharField(max_length=30) 
 
-class typeStage(models.Model):
-    typeStage=models.CharField(max_length=30)
-    duree=models.IntegerField
-
-class Stage(models.Model):
-    nomStage=models.CharField(max_length=120)
-    typeStage=models.ForeignKey(typeStage, on_delete=models.CASCADE)
-    idOrganisme=models.ForeignKey(Organisme, on_delete=models.CASCADE)
